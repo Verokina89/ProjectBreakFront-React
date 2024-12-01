a692014bd0d26d75cd32764779cb76eb05d269cd
Verónica I Pérez S <v.isabel1989@gmail.com>
2024-12-01 12:12:25 +0100

    creacion de home y login coomo pruebas.Creamos Hooks para manejar las solicitudes(UseFetch) y para manejar los formularios(UseForm)

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p> Acceso solo para logedos.</p>
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Inicio</h1>
+      <p>Bienvenido a la tienda de Kia y Canela; las pug calino mas monas.</p>
+    </div>
+  );
+};
+
+export default Home;
