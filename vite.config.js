5d939912aeba8a74846919db7d01bc8810efc0dd
Verónica I Pérez S <v.isabel1989@gmail.com>
2024-12-02 00:18:18 +0100

    modificacion de Dasssshnoard. Prueba de codigo tras modificar nuevamente vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'firebase/auth': 'firebase/auth/dist/index.esm.js',
-    },
+  optimizeDeps: {
+    include: ['firebase/auth'],
   },
+ 
 })
