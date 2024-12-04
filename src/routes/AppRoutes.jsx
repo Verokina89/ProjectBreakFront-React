021b7707d5984ff3e9ef0f2b49fdc5664402acbb
Verónica I Pérez S <v.isabel1989@gmail.com>
2024-12-04 01:23:22 +0100

    modificacion en AppRoutes.js para importar Category y Contact

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,11 +3,13 @@ import PrivateRoute from '../components/PrivateRoute';
 import Dashboard from '../pages/Dashboard';
 import Login from '../pages/Login';
 import Category from '../pages/Category';
+import Contact from '../pages/Contact';
 
 function AppRoutes() {
     return (
         <Routes>
             <Route path="/category" element={<Category />} />
+            <Route path="/contacto" element={<Contact />} />
             <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
             <Route path="/login" element={<Login />} />
         </Routes>
